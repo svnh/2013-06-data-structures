da7d2d3e8c24a0ed899c82f0f81d4b94d2764912
Hack Reactor Students <github@hackreactor.com>
2013-06-19 16:27:31 -0700

    queue and queuespec finished

--- spec/queueSpec.js
+++ spec/queueSpec.js
@@ -14,12 +14,67 @@ describe("queue", function() {
   });
   /* END DO NOT MODIFY */
 
-  // Any queue implementation should have the following methods
+  // queue implementation should have the following methods
   it('should have "enqueue", "dequeue", and "size" methods', function() {
     expect(queue.enqueue).to.be.a('function');
     expect(queue.dequeue).to.be.a('function');
     expect(queue.size).to.be.a('function');
   });
 
-  // Hey! Add tests here that thoroughly test the functionality of your queue
+  it('if storage is empty then size should return 0', function() {
+      expect(queue.size()).to.equal(0);
+    });
+
+  it('if enqueue size will increase by 1', function() {
+    queue.enqueue('bill');
+    expect(queue.size()).to.equal(1);
+  });
+
+  it('should add enqueued items to the end of the queue', function() {
+    // Fill out the body of the test here
+    queue.enqueue('hi');
+    queue.enqueue('bye');
+    queue.enqueue('good morning');
+    queue.enqueue('good night');
+    console.log(queue);
+    console.log(queue.storage());
+    storage=queue.storage();
+    expect(storage[queue.size()]).to.equal('good night');
+  });
+
+it('dequeue should return the first element in queue', function() {
+    queue.enqueue('john');
+    queue.enqueue('bill');
+    queue.enqueue('joe');
+    expect(queue.dequeue()).to.equal('john');
+});
+
+  it('should not error when dequeuing from an empty queue', function() {
+    queue.dequeue();
+    expect(queue.size()).to.equal(0);
+    // Fill out the body of the test here
+  });
+
+  it('should remove dequeued items from the front of the stack', function() {
+    // Fill out the body of the test here
+    queue.enqueue('hi');
+    queue.enqueue('bye');
+    queue.enqueue('good morning');
+    queue.enqueue('good night');
+    expect(queue.dequeue()).to.equal('hi');
+
+  });
+
+  it('should enqueue and dequeue multiple items in the right order (FIFO)', function() {
+    // Fill out the body of the test here
+    queue.enqueue('bob');
+    queue.enqueue('joe');
+    queue.enqueue('john');
+    queue.enqueue('eric');
+    expect(queue.dequeue()).to.equal('bob');
+    queue.enqueue('good afternoon');
+    expect(queue.dequeue()).to.equal('joe');
+  });
+//[person1, person2, 3, 4];
+
 });
--- src/functional/queue.js
+++ src/functional/queue.js
@@ -2,18 +2,36 @@ var makeQueue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   var queue = {};
 
   queue.enqueue = function(value){
+    size += 1;
+    storage[size] = value;
   };
 
   queue.dequeue = function(){
+    var temp = storage[1];
+    // delete storage[1];
+    var n = size;
+    for (var key in storage) {
+      storage[key] = storage[parseInt(key, 10)+1];
+    }
+
+    if (size > 0) {
+      size -= 1;
+    }
+    return temp;
   };
 
   queue.size = function(){
+    return size;
+  };
+
+  queue.storage=function(){
+    return storage;
   };
 
   return queue;
