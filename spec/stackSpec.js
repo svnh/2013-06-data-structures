a5536321f4cedb875c96e13024e38c4269364f47
Hack Reactor Students <github@hackreactor.com>
2013-06-19 14:54:03 -0700

    stack and stackspec done

--- spec/stackSpec.js
+++ spec/stackSpec.js
@@ -23,19 +23,72 @@ describe("stack", function() {
 
   it('should add pushed items to the top of the stack', function() {
     // Fill out the body of the test here
+    stack.push('hi');
+    stack.push('bye');
+    stack.push('good morning');
+    stack.push('good night');
+    expect(stack[3]).to.equal('good morning');
+    expect(stack[stack.size()]).to.equal('good night');
+
+
   });
 
   it('should remove popped items from the top of the stack', function() {
     // Fill out the body of the test here
+    stack.push('hi');
+    stack.push('bye');
+    stack.push('good morning');
+    stack.push('good night');
+    stack.pop();
+    expect(stack.pop()).to.equal('good morning');
+
   });
 
   it('should push and pop multiple items in the right order (LIFO)', function() {
     // Fill out the body of the test here
+    stack.push('hi');
+    stack.push('bye');
+    stack.push('good morning');
+    stack.push('good night');
+    expect(stack.pop()).to.equal('good night');
+    stack.push('good afternoon');
+    expect(stack.pop()).to.equal('good afternoon');
+
+
   });
 
   it('should not error when popping from an empty stack', function() {
+    stack.pop();
+    expect(stack.size()).to.equal(0);
     // Fill out the body of the test here
   });
 
   // Hey! Add more tests here if you can think of ways to test your stack more thoroughly
+
+  it('if storage is empty then size should return 0', function() {
+    expect(stack.size()).to.equal(0);
+    // Fill out the body of the test here
+  });
+
+  it('if we push to stack size will increase by 1', function() {
+    stack.push('hi');
+    expect(stack.size()).to.equal(1);
+    // Fill out the body of the test here
+  });
+
+  it('if stack size > 0 and we pop then size decreases by 1', function() {
+    stack.push('hi');
+    stack.pop();
+    expect(stack.size()).to.equal(0);
+    // Fill out the body of the test here
+  });
+
+it('pop should return the last element that was pushed', function() {
+    stack.push('hi');
+    expect(stack.pop()).to.equal('hi');
+    // Fill out the body of the test here
+  });
+
+
 });
+
--- src/functional/stack.js
+++ src/functional/stack.js
@@ -2,18 +2,27 @@ var makeStack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
   var stack = {};
 
   stack.push = function(value){
+    size += 1;
+    stack[size] = value;
   };
 
   stack.pop = function(){
+    var temp = stack[size];
+    delete stack[size];
+    if (size > 0) {
+      size -= 1;
+    }
+    return temp;
   };
 
   stack.size = function(){
+    return size;
   };
 
   return stack;
