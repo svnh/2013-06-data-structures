78d846ae9435dbb0a8e4cc7697da2e226ef947d1
Hack Reactor Students <github@hackreactor.com>
2013-06-19 20:30:29 -0700

    pseudoclassical works

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,3 +1,39 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+var Queue = function(){
+  // Use an object with numeric keys to store values
+  //var storage = {};
+
+//  var size = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+    this.storage = {};
+    this.size = 0;
 };
+
+
+Queue.prototype.enqueue = function(value){
+  this.size += 1;
+  this.storage[this.size] = value;
+};
+
+Queue.prototype.dequeue = function(){
+  var temp = this.storage[1];
+  // delete storage[1];
+  var n = this.size;
+  for (var key in this.storage) {
+    this.storage[key] = this.storage[parseInt(key, 10)+1];
+  }
+
+  if (this.size > 0) {
+    this.size -= 1;
+  }
+  return temp;
+};
+
+Queue.prototype.returnsize = function(){
+  return this.size;
+};
+
+Queue.prototype.storage=function(){
+  return this.storage;
+};
+
--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,3 +1,28 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(){
+  // Use an object with numeric keys to store values
+   // Hint: set an initial value here
+
+  // Implement the methods below
+  this.storage = {};
+  this.size = 0;
 };
+
+Stack.prototype.push = function(value){
+  this.size += 1;
+  this.storage[this.size] = value;
+};
+
+Stack.prototype.pop = function(){
+  var temp = this.storage[this.size];
+  delete this.storage[this.size];
+  if (this.size > 0) {
+    this.size -= 1;
+  }
+  return temp;
+};
+
+Stack.prototype.returnsize = function(){
+  return this.size;
+};
+
+
